7a26b165ad3e078be836d16f3deb653c7adf21fd
Abraham Post <abrahampost@yahoo.com>
2018-08-17 00:43:51 -0400

    Recursive descent Parser implemented

--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -7,7 +7,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-
 public class Lox {
 
 	static boolean hadError;
@@ -42,9 +41,11 @@ public class Lox {
 	private static void run(String source) {
 		Scanner scanner = new Scanner(source);
 		List<Token> tokens = scanner.scanTokens();
-		for (Token token : tokens) {
-			System.out.println(token);			
-		}
+		Parser parser = new Parser(tokens);
+		Expr expression = parser.parse();
+		
+		if (hadError) return;
+		System.out.println(new AstPrinter().print(expression));
 	}
 	
 	static void error(int line, String message) {
@@ -55,6 +56,14 @@ public class Lox {
 		System.err.println(
 				"[line " + line + "] Error" + where + ": " + message);
 		hadError = true;
+	}
+	
+	static void error(Token token, String message) {
+		if (token.type == TokenType.EOF) {
+			report(token.line, " at end", message);
+		} else {
+			report(token.line, " at '" + token.lexeme + "'", message);
+		}
 	}
 
 }
