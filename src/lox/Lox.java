0c7f8565578ac83b3f287cfbefd469100a2363fc
Abraham <abrahampost@yahoo.com>
2018-08-17 23:45:04 -0400

    Implemented interpreter (Ch. 7)

--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -9,7 +9,9 @@ import java.util.List;
 
 public class Lox {
 
-	static boolean hadError;
+	private static final Interpreter interpreter = new Interpreter();
+	static boolean hadError = false;
+	static boolean hadRuntimeError = false;
 
 	public static void main(String[] args) throws IOException {
 		if (args.length > 1) {
@@ -25,7 +27,8 @@ public class Lox {
 	private static void runFile(String path) throws IOException {
 		byte[] bytes = Files.readAllBytes(Paths.get(path));
 		run(new String(bytes, Charset.defaultCharset()));
-		if(hadError) System.exit(65);
+		if (hadError) System.exit(65);
+		if (hadRuntimeError) System.exit(70);
 	}
 	
 	private static void runPrompt() throws IOException {
@@ -45,7 +48,8 @@ public class Lox {
 		Expr expression = parser.parse();
 		
 		if (hadError) return;
-		System.out.println(new AstPrinter().print(expression));
+		
+		interpreter.interpret(expression);
 	}
 	
 	static void error(int line, String message) {
@@ -64,6 +68,12 @@ public class Lox {
 		} else {
 			report(token.line, " at '" + token.lexeme + "'", message);
 		}
+	}
+	
+	static void runtimeError(RuntimeError error) {
+		System.err.println(error.getMessage() +
+				"\n[line " + error.token.line + "]");
+		hadRuntimeError = true;
 	}
 
 }
